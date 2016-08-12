dd754743e3fe92dc74c144672b88f3d17d39b7a8
Elliot Wright <wright.elliot@gmail.com>
2016-08-12 16:03:52 +0100

    Forgot to run CMD at end of entrypoint...

--- provisioning/docker-entrypoint.sh
+++ provisioning/docker-entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -e
+
 # The home directory will hopefully be in either /home or /Users. The jenkins user has access to
 # create directories there.
 mkdir -p ${SLAVE_HOME}
 
 # Set new home directory for using ~.
 export HOME=${SLAVE_HOME}
+
+exec "$@"
