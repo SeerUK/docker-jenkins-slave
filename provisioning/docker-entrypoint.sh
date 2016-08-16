7dfe7740a114bed8f322be3aabf4acae756beeda
Elliot Wright <wright.elliot@gmail.com>
2016-08-16 12:41:24 +0100

    Updating user creation.

--- provisioning/docker-entrypoint.sh
+++ provisioning/docker-entrypoint.sh
@@ -2,15 +2,25 @@
 
 set -e
 
-# Create the user with the given home directory.
-echo "Creating user with UID:GID of ${JENKINS_UID}:${JENKINS_GID}"
-groupadd -g ${JENKINS_GID} -o jenkins
-useradd -s /bin/bash -u ${JENKINS_UID} -g ${JENKINS_GID} -d ${JENKINS_HOME} -M -o jenkins
+USER=$(getent passwd jenkins 2>&1)
+GROUP=$(getent group jenkins 2>&1)
 
-# Create the home directory, if it doesn't already exist. We won't bother with the skeleton.
-mkdir -p ${JENKINS_HOME}
+if [ ${#USER} != 0 ] && [ ${#GROUP} != 0 ]; then
+    # Create the user with the given home directory.
+    echo "Creating user with UID:GID of ${JENKINS_UID}:${JENKINS_GID}"
+    groupadd -g ${JENKINS_GID} -o jenkins
+    useradd -s /bin/bash -u ${JENKINS_UID} -g ${JENKINS_GID} -d ${JENKINS_HOME} -M -o jenkins
+fi
 
 # Set new home directory for using ~.
 export HOME=${JENKINS_HOME}
+
+# Create the home directory, if it doesn't already exist. We won't bother with the skeleton.
+mkdir -p ${JENKINS_HOME}
+chown -R ${JENKINS_UID}:${JENKINS_GID} ${JENKINS_HOME}
+
+# Create the workspace.
+mkdir -p ${SLAVE_WORKSPACE}
+chown -R ${JENKINS_UID}:${JENKINS_GID} ${SLAVE_WORKSPACE}
 
 exec gosu jenkins "$@"
