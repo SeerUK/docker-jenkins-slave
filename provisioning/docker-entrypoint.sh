08e66da70fdd6dfd222a8337db4944a0fbedfce4
Elliot Wright <wright.elliot@gmail.com>
2016-08-16 10:58:10 +0100

    Using gosu to handle switching user and whatnot.

--- provisioning/docker-entrypoint.sh
+++ provisioning/docker-entrypoint.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-# The home directory will hopefully be in either /home or /Users. The jenkins user has access to
-# create directories there.
-mkdir -p ${SLAVE_HOME}
+# Create the user with the given home directory.
+echo "Creating user with UID:GID of ${JENKINS_UID}:${JENKINS_GID}"
+useradd -s /bin/bash -u ${JENKINS_UID} -g ${JENKINS_GID} -d ${JENKINS_HOME} -m -o jenkins
 
 # Set new home directory for using ~.
-export HOME=${SLAVE_HOME}
+export HOME=${JENKINS_HOME}
 
-exec "$@"
+exec gosu jenkins "$@"
--- provisioning/start-slave.sh
+++ provisioning/start-slave.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-cd ${SLAVE_WORKSPACE}
-
 # Attempt to wait for host to be ready...
 while ! ping -c1 ${MASTER_HOST} > /dev/null; do sleep 0.5; done
 
 # Download the slave.jar file from the master node.
-curl -O http://${MASTER_HOST}:${MASTER_PORT}/jnlpJars/slave.jar
+curl -o ${SLAVE_WORKSPACE}/slave.jar http://${MASTER_HOST}:${MASTER_PORT}/jnlpJars/slave.jar
 
 # Start the slave, connecting the the master to retrieve configuration.
-java ${JAVA_OPTS} -jar slave.jar -jnlpUrl http://${MASTER_HOST}:${MASTER_PORT}/computer/${SLAVE_NAME}/slave-agent.jnlp
+java ${JAVA_OPTS} -jar ${SLAVE_WORKSPACE}/slave.jar -jnlpUrl http://${MASTER_HOST}:${MASTER_PORT}/computer/${SLAVE_NAME}/slave-agent.jnlp
