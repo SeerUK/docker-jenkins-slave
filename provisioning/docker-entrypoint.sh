78ed8440dd4be0116ba17899e1e35bfc4010c797
Elliot Wright <wright.elliot@gmail.com>
2016-08-16 11:17:52 +0100

    Create group too.

--- provisioning/docker-entrypoint.sh
+++ provisioning/docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 # Create the user with the given home directory.
 echo "Creating user with UID:GID of ${JENKINS_UID}:${JENKINS_GID}"
+groupadd -g ${JENKINS_GID} -o jenkins
 useradd -s /bin/bash -u ${JENKINS_UID} -g ${JENKINS_GID} -d ${JENKINS_HOME} -m -o jenkins
 
 # Set new home directory for using ~.
