0f003f72247582a6ab49f2af2142753ba7aa838d
Elliot Wright <wright.elliot@gmail.com>
2016-08-16 14:16:12 +0100

    Updating start slave script.

--- provisioning/start-slave.sh
+++ provisioning/start-slave.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-# Wait a little for Jenkins to start before trying to ping it...
-sleep 5
+echo "==> Waiting for Jenkins container to start..."
+while ! ping -c1 ${MASTER_HOST} > /dev/null; do
+    sleep 0.5;
+    echo "    Jenkins container still not ready..."
+done
+echo "    Jenkins container is available!"
 
-# Attempt to wait for host to be ready...
-while ! ping -c1 ${MASTER_HOST} > /dev/null; do sleep 0.5; done
+echo "==> Sleeping for 20 seconds to wait for Jenkins boot..."
+sleep 20;
 
-# Download the slave.jar file from the master node.
+echo "==> Downloading slave JAR from master node..."
 curl -o ${SLAVE_WORKSPACE}/slave.jar http://${MASTER_HOST}:${MASTER_PORT}/jnlpJars/slave.jar
+echo "    Done!"
 
 # Start the slave, connecting the the master to retrieve configuration.
+echo "==> Starting slave..."
 java ${JAVA_OPTS} -jar ${SLAVE_WORKSPACE}/slave.jar -jnlpUrl http://${MASTER_HOST}:${MASTER_PORT}/computer/${SLAVE_NAME}/slave-agent.jnlp
