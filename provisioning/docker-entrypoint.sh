1fc91ec48939f7fd054851625513c67516ab5067
Elliot Wright <wright.elliot@gmail.com>
2016-08-16 12:09:48 +0100

    Don't make home folder when creating user.

--- provisioning/docker-entrypoint.sh
+++ provisioning/docker-entrypoint.sh
@@ -5,7 +5,10 @@ set -e
 # Create the user with the given home directory.
 echo "Creating user with UID:GID of ${JENKINS_UID}:${JENKINS_GID}"
 groupadd -g ${JENKINS_GID} -o jenkins
-useradd -s /bin/bash -u ${JENKINS_UID} -g ${JENKINS_GID} -d ${JENKINS_HOME} -m -o jenkins
+useradd -s /bin/bash -u ${JENKINS_UID} -g ${JENKINS_GID} -d ${JENKINS_HOME} -M -o jenkins
+
+# Create the home directory, if it doesn't already exist. We won't bother with the skeleton.
+mkdir -p ${JENKINS_HOME}
 
 # Set new home directory for using ~.
 export HOME=${JENKINS_HOME}
